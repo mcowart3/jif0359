c75b0a9375f3fc8ea03c9a0538215630b402391a
srobinson75 <50890713+srobinson75@users.noreply.github.com>
2021-04-20 21:54:36 -0400

    Revert "Revert "Runs the project completely using docker""

--- frontEnd/src/services/commentService/LocalCommentService.ts
+++ frontEnd/src/services/commentService/LocalCommentService.ts
@@ -3,11 +3,10 @@ import LitDocument from "../../model/LitDocument";
 import ICommentService from "./ICommentService";
 
 export default class LocalCommentService implements ICommentService {
-	public async getComments(document: LitDocument): Promise<DocComment[]> {
-		let comments = await (await fetch(
-			"http://localhost:3000/", { mode: 'no-cors' })).json();
-		console.log(comments);
-		comments = null; //TODO BE PERSON
-		return new Promise((resolve, reject) => resolve(comments));
-	}
+  public async getComments(document: LitDocument): Promise<DocComment[]> {
+    let comments = await (await fetch("http://localhost:5000/")).json();
+    console.log(comments);
+    comments = null; //TODO BE PERSON
+    return new Promise((resolve, reject) => resolve(comments));
+  }
 }
